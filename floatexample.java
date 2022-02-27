80b19b9f155462986100d9e4e4df9325094e95e4
Ginja22 <88261803+Ginja22@users.noreply.github.com>
2022-02-26 18:11:55 -0600

    Add files via upload
    Here's a simple float example.

---
+++ floatexample.java
@@ -0,0 +1,9 @@
+package com.floatexample.java;
+
+public class floatexample {
+        public static void main(String [] args){
+            Float number = 42.7f;
+            System.out.println(number);
+    
+    }
+}
